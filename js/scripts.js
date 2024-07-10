c1d78ddca381c3a35e3f279ad9a3a42a2a87fa20
Eduard-Condurache <eduardcondurache96@gmail.com>
2024-07-10 16:14:00 +0200

    Completato esercizio

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+const name = prompt('Quale è il tuo nome?');
+const surName = prompt('Quale è il tuo cognome?');
+const color = prompt('Quale è il tuo colore preferito?');
+
+console.log(name + ' ' + surName + ' ' + color + ' ' + '130');
+
+document.writeln(name + ' ' + surName + ' ' + color + ' ' + '130');
+
