c42665e93e2d823b9db9116630a3f946acdef552
Eduard-Condurache <eduardcondurache96@gmail.com>
2024-07-11 10:27:36 +0200

    Sistemato esercizio

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,16 @@
-const name = prompt('Quale è il tuo nome?');
-const surName = prompt('Quale è il tuo cognome?');
+const firstName = prompt('Quale è il tuo nome?');
+console.log("Il nome dell utente è:", firstName);
+
+const lastName = prompt('Quale è il tuo cognome?');
+console.log("Il cognome dell utente è:", lastName);
+
 const color = prompt('Quale è il tuo colore preferito?');
+console.log("Il colore preferito dell utente è:", color);
 
-console.log(name + ' ' + surName + ' ' + color + ' ' + '130');
 
-document.writeln(name + ' ' + surName + ' ' + color + ' ' + '130');
+const password = firstName + lastName + color + '130'
+console.log("La password è:", firstName + lastName + color + '130');
+
+
+document.getElementById('password-container').innerHTML = password;
 
